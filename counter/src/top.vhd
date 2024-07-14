ab73ac740dbf0d5edb68235bd02fd95789544f4f
Fabien Marteau <mail@fabienm.eu>
2024-07-14 21:29:10 +0200

    counter portage

--- counter/src/top.vhd
+++ counter/src/top.vhd
@@ -39,15 +39,67 @@ use ieee.numeric_std.all;
 
 entity top is
   port (
-    clk : in std_logic;
-    led : out std_logic_vector(7 downto 0)
+    ext_clk : in std_logic;
+    led : out std_logic_vector(7 downto 0);
+    rst : in std_logic -- /!\ inversé -> rst_n
   );
 end top;
 
 architecture arch of top is
+
+  component CC_PLL is
+  generic (
+      REF_CLK         : string;  -- reference input in MHz
+      OUT_CLK         : string;  -- pll output frequency in MHz
+      PERF_MD         : string;  -- LOWPOWER, ECONOMY, SPEED
+      LOW_JITTER      : integer; -- 0: disable, 1: enable low jitter mode
+      CI_FILTER_CONST : integer; -- optional CI filter constant
+      CP_FILTER_CONST : integer  -- optional CP filter constant
+  );
+  port (
+      CLK_REF             : in  std_logic;
+      USR_CLK_REF         : in  std_logic;
+      CLK_FEEDBACK        : in  std_logic;
+      USR_LOCKED_STDY_RST : in  std_logic;
+      USR_PLL_LOCKED_STDY : out std_logic;
+      USR_PLL_LOCKED      : out std_logic;
+      CLK0                : out std_logic;
+      CLK90               : out std_logic;
+      CLK180              : out std_logic;
+      CLK270              : out std_logic;
+      CLK_REF_OUT         : out std_logic
+  );
+  end component;
+ 
   signal n : natural range 0 to 255;
   signal cen : std_logic;
+  signal clk : std_logic;
+
 begin
+
+  socket_pll : CC_PLL
+  generic map (
+      REF_CLK         => "10.0",
+      OUT_CLK         => "100.0",
+      PERF_MD         => "ECONOMY",
+      LOW_JITTER      => 1,
+      CI_FILTER_CONST => 2,
+      CP_FILTER_CONST => 4
+  )
+  port map (
+      CLK_REF             => ext_clk,
+      USR_CLK_REF         => '0',
+      CLK_FEEDBACK        => '0',
+      USR_LOCKED_STDY_RST => '0',
+      USR_PLL_LOCKED_STDY => open,
+      USR_PLL_LOCKED      => open,
+      CLK0                => clk,
+      CLK90               => open,
+      CLK180              => open,
+      CLK270              => open,
+      CLK_REF_OUT         => open
+  );
+
   clock_divider : entity work.clock_divider
   generic map (DIVISOR => 5000000)
   port map (clk => clk, cen => cen);
@@ -55,7 +107,10 @@ begin
   counter : process(clk)
   begin
     if rising_edge(clk) then
-      if cen = '1' then
+      if rst = '0' then
+        led <= x"AA";
+        n <= 0;
+      elsif cen = '1' then
         n <= n + 1;
         led <= std_logic_vector(to_unsigned(n, led'length));
       end if;
